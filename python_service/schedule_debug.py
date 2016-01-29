0e5d980f19932a9dbf883fcc3697dc463f687987
SmartRiver <johnsonhe0306@gmail.com>
2016-01-29 18:40:58 +0800

    微调 -- 2016/01/29

--- python_service/schedule_debug.py
+++ python_service/schedule_debug.py
@@ -4,13 +4,15 @@ __doc__     =  '''this py is used to schedule the timetable for those who want t
                 the input is the grade、 target school level、 condition of the user，
                 the output is the schedule describing what they should prepare in different term'''
 SEG_TARGET_DICT = {
-    u
 }
 
 
 def schedule():
-
+    pass
 def __init__():
+    for each in open('resource/schedule/target.csv', 'r',):
+        each = each.strip('\r').strip('\n')
+        SEG_TARGET_DICT[]
 
 
 __init__()
