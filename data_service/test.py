e3e83ef2008fc84b14a5a8a48f91d8c3b21057d2
XiaoyuGuo <11301007@bjtu.edu.cn>
2016-03-04 13:03:49 +0800

    bug

--- data_service/test.py
+++ data_service/test.py
@@ -14,11 +14,9 @@ import time
 import json
 import logging
 
-<<<<<<< HEAD
-student_info = {"major":"materials","grade":"3","target":"2","data":{"gpa":{"score":"3.8", "school":"9"},"gre":{"total":"340","verbal":"170","writing":"0"},"gmat":{"total":"800","writing":"5"},"toefl":{"total":"120", "writing":"30", "reading":"", "listening":"", "speaking":"30"},"ielts":{"total":"9", "writing":"", "reading":"","listening":"", "speaking":"9"},"research":{"duration":"4", "level":"2", "achievement":"2", "recommendation":"1"},"work":{"duration":"1", "level":"1", "recommendation":"1"},"internship":{"duration":"1", "level":"1", "recommendation":"1"},"activity":{"duration":"1", "type":"1"},"competition":{"level":"2"},"scholarship":{"level":"7"}}}
-=======
+
 student_info = {"major":"cs","grade":"3","target":"2","data":{"gpa":{"score":"3.5", "trend":"2", "school":"9"},"gre":{"total":"320","verbal":"160","writing":"5"},"toefl":{"total":"110", "writing":"", "reading":"", "listening":"", "speaking":""},"ielts":{"total":"6", "writing":"", "reading":"","listening":"", "speaking":""},"research":{"duration":"4", "level":"2", "achievement":"2", "recommendation":"1"},"work":{"duration":"1", "level":"1", "recommendation":"1"},"internship":{"duration":"5", "level":"4", "recommendation":"1"},"reletter":{"level":["1","2","3"]},"activity":{"duration":"4", "type":"5"},"credential":{"level":"4"},"competition":{"level":"5"},"scholarship":{"level":"7"}}}
->>>>>>> 4ad9f633b362a5014bed63219e1697d44e122485
+
 
 
 assess_student.init()
